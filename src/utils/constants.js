2f68679d9e32d0d0b44d19f0a076d66feaa1e0ed
sitansuss <75010682+sitansuss@users.noreply.github.com>
2023-10-31 00:59:53 +0530

    tmdb api key change

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -6,7 +6,7 @@ export const API_OPTIONS = {
   headers: {
     accept: "application/json",
     Authorization:
-      "Bearer " + process.env.REACT_APP_OPENAI_KEY,
+      "Bearer " + process.env.REACT_APP_TMDB_KEY,
   },
 };
 
